050d9becca72d9ff133e9c948db89c3c083c0a95
Jeson <jesonpl2006@gmail.com>
2025-12-13 23:57:01 +0000

    Experiments setup

--- run_experiments.m
+++ run_experiments.m
@@ -7,26 +7,17 @@ theta_data = out.data_theta;
 alpha_data = out.data_alpha;
 u_data = out.data_u;
 
-% 2. Define the Folder Name
 folder_name = 'experiment_csv';
-
-% Check if the folder exists; if not, create it automatically
 if ~exist(folder_name, 'dir')
     mkdir(folder_name);
 end
-
-% 3. Ask for a Filename
 run_name = input('Enter name for this file: ', 's');
 
-% --- NEW: Construct the full path ---
-% fullfile combines the folder and filename safely
-filename = fullfile(folder_name, strcat(run_name, '.csv'));
 
-% 4. Create a Table
+filename = fullfile(folder_name, strcat(run_name, '.csv'));
 Data = table(t_data, theta_data, alpha_data, u_data, ...
     'VariableNames', {'Time', 'Theta', 'Alpha', 'Input'});
 
-% 5. Write to CSV file in the specific folder
 writetable(Data, filename);
 
 fprintf('SUCCESS: Data saved to "%s".\n', filename);
